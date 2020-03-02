da5732241d370e6bfc17ff1d7cdeb48a803f8b0e
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-02 02:17:33 +0000

    added cars and

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//project is backend
+
 const express = require('express');
 const app = express();//top-level function of express
 
 const path = require('path');//*
-const apiData = require('./')
+const apiData = require('./people.json');
+
+const carOwners = require('./car-owners.json');
+const cars = require('./cars.json');
+
 const port = 3000;
 
 app.use((req,res,next)=>{
@@ -12,24 +18,76 @@ app.use((req,res,next)=>{
 
 
 //used to set default message before routing
-// app.get('/', (req, res) => res.send('good morning queen'));
+// app.get('/', (req, res) => res.send('good morning queen'));send is used in express
 
 app.use(express.static('public'));//all files from public folder must be included - static means we are not going to change them .
-app.use('/boostrap',express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
+app.use('/bootstrap',express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
 app.use('/jquery',express.static(path.join(__dirname,'node_modules/jquery/dist')));
 app.use('/popper',express.static(path.join(__dirname,'node_modules/@popperjs/core/dist/umd')));
-app.use('/js',express.static(path.join(__dirname,'pubic/js')));
+app.use('/js',express.static(path.join(__dirname,'public/js')));
+app.use('/css',express.static(path.join(__dirname,'public/css')));
+
+
 // set the route for index.html
 app.get('/',(req,res,)=>{
   res.sendFile(path.join(__dirname+'/public/index.html'));//sending file to the browser , joins the set of paremeters you give
 })
+
 // set the route for about.html
 app.get('/',(req,res,)=>{
     res.sendFile(path.join(__dirname+'/public/about.html'));
 })// in order to use path make sure you set the path before hand look at *
 
-app.get('/people',(req,res,)=>{
+// set the route for people.json has data from mockaroo
+//give access to apiData
+app.get('/people', (req,res,)=>{
   res.json(apiData);
-})
+});
+
+app.get('/gender/g=:gender',(req,res)=>{
+  const genderParam = req.params.gender; //retrieves the parameter value requested by the user
+  if ((genderParam === 'male') || (genderParam === 'female')){
+    let filteredArray = [];//array to push the matching objects to user's value
+    for (let i = 0; i < apiData.length; i++) {
+      if (genderParam === apiData[i].gender.toLowerCase()){
+        filteredArray.push(apiData[i]);
+      }
+    }
+    res.send(filteredArray);
+  } else {
+    res.send('Invalid parameter');
+  }
+});
+
+
+app.get('/carOwners/ownscar=:owns_car',(req,res)=>{
+  const ownsParam = req.params.owns_car;
+
+  let ownersFilteredArray = [];//array to push the matching objects to user's value
+
+  for (let i = 0; i < carOwners.length; i++) {
+    if (ownsParam.toLowerCase() === carOwners[i].owns_car.toLowerCase()) {
+      ownersFilteredArray.push(carOwners[i]);
+    }
+  }
+  res.send(ownersFilteredArray);
+
+
+});
+
+
+app.get('/carOwners/fname=:first_name&ownscar=:owns_car',(req,res)=>{
+  const nameParam = req.params.first_name;
+  const carParam = req.params.owns_car; //retrieves the parameter value requested by the user
+
+  let ownersFilteredArray = [];//array to push the matching objects to user's value
+  for (let i = 0; i < carOwners.length; i++) {
+    if ((nameParam.toLowerCase() === carOwners[i].first_name.toLowerCase()) && (carParam.toLowerCase() === carOwners[i].owns_car.toLowerCase())){
+      ownersFilteredArray.push(carOwners[i]);
+    }
+  }
+  res.send(ownersFilteredArray);
+
+  });
 
 app.listen(port, () => console.log(`This app is listening on port ${port}!`))
